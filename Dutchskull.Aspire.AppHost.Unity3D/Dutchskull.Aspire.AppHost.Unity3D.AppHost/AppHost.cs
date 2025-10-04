2687f02f467f0105a93c16ea22bd3a13456b6f22
Bastien Olivier Dijkstra <Bas10olivier@hotmail.com>
2025-10-04 17:04:54 +0200

    fix: add environment variable to test with

--- Dutchskull.Aspire.AppHost.Unity3D/Dutchskull.Aspire.AppHost.Unity3D.AppHost/AppHost.cs
+++ Dutchskull.Aspire.AppHost.Unity3D/Dutchskull.Aspire.AppHost.Unity3D.AppHost/AppHost.cs
@@ -7,6 +7,7 @@ var api = builder.AddProject<Dutchskull_Aspire_Apphost_Unity3D_Api>("api");
 var unity = builder
     .AddUnityProject("game", "..\\..\\AspireIntegration", 1, customUnityInstallRoot: "E:\\Unity")
     .WithReference(api)
+    .WithEnvironment("Test", "Value")
     .WaitFor(api);
 
 var container = builder.AddContainer("test", "docker/welcome-to-docker");
